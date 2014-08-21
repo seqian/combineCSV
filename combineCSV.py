9ab5ab16321f1238f6e4b6b45a6dbe6cba08a387
Seqian Wang <seqian@outlook.com>
2014-08-21 17:36:48 -0400

    Integrated capacity to compare using visCode in addition to rid.

--- combineCSV.py
+++ combineCSV.py
@@ -1,100 +1,111 @@
 #! /usr/bin/env python
 # Read list of patients, go through dataList and retain only necessary patients
 # Works on Python 3 only, not Python 2
-# Version 2014-08-06
+# Version 2014-08-21
 
 ##### Parameters you can have fun playing with
-subjectList = "subjectsList.csv" # Will retrieve the list of rid's from this csv file.
-dataList = "dataList.csv" # Will find the rows corresponding to the list of rid's from the subjectsList file.
-outputList = "outputList.csv" # Will write the rows to this file.
-keepAll = False # True or False. Will keep all the rows in dataList that correspond to a rid from subjectList. Note that if True you will have to manually choose which row you want to keep and delete.
-skipLineIfNotFound = True # True or False. If True, will skip lines in outputList to align the rids with the subjectList.
-headerString = 'rid' # Here, you can define the header of the column you want to use to match the two lists
+compareVisCode = True
+keepAll = False
+
+### Options Instructions
+# subjectsList specifies the csv file with the list of subjects.
+# dataList specifies the csv file with the data.
+# outputList specifies the output csv file.
+# compareVisCode. If enabled, it will only match rows with the same visit code. Especially useful with ADNI.
+# keepAll determines whether to keep all the rows in dataList that correspond to a rid from subjectList. \
+# Note that if True you will have to manually choose which row you want to keep and delete.
+# skipLineIfNotFound determines whether to skip lines in outputList to align the rows with rids.
+
 
 ##### The code which you shall not touch without my permission (but feel free to gaze at its ingenuity)
-import csv, random
+import csv
+subjectList = "subjectsList.csv"
+dataList = "dataList.csv"
+outputList = "outputList.csv"
+
 
 ### Functions
-def csvObject(csvfile):
+def csv_object(csvfile):
     try:
         check = csvfile.read(1024)
-        dialect = csv.Sniffer().sniff(check,delimiters=',\t ;:.')
-        csvfile.seek(0) # Goes back to beginning
+        dialect = csv.Sniffer().sniff(check, delimiters=',\t ;:.')
+        csvfile.seek(0)  # Goes back to beginning
         reader = csv.reader(csvfile, dialect)
     except:
-        csvfile.seek(0) # Goes back to beginning
+        csvfile.seek(0)  # Goes back to beginning
         reader = csv.reader(csvfile, delimiter=',')
     return reader
 
-def findRIDcolumn(header, fileName):
-    whereIsRID = 0 # First Column by Default
+
+def find_column(header, header_string):
+    column_number = 0  # First Column by Default
     found = False
-    for i in range(len(header)): #For each column in the header
-        if headerString in header[i].strip().lower(): # If the column's header is RID
-            whereIsRID = i
+    for i in range(len(header)):  # For each column in the header
+        if header_string in header[i].strip().lower():  # If the column's header is RID
+            column_number = i
             found = True
             break
     if not found:
-        print("No header with ", headerString, " in ", fileName,". I will assume rid's are in the first column.")
-        whereIsRID = 0 # First Column by Default
-    return whereIsRID
+        column_number = 0  # First Column by Default
+    return column_number
 
-def bleachRID(rid):
-    #newRid = rid.strip().lstrip('0')
-    newRid = ''.join(i for i in rid if i.isdigit()).lstrip('0')
-    return newRid
 
-def listRID(csvfile):
-    list = []
-    reader = csvObject(csvfile)
+def bleach_rid(rid):
+    rid = ''.join(i for i in rid if i.isdigit()).lstrip('0')
+    return rid
+
+
+def list_rid(csvfile):
+    csv_list = []
+    reader = csv_object(csvfile)
     for row in reader:
-        list.append(row)
-    return list
+        csv_list.append(row)
+    return csv_list
 
 # Read rid's
 with open(subjectList, 'U') as csvfile:
-    data1 = listRID(csvfile)
+    data1 = list_rid(csvfile)
 with open(dataList, 'U') as csvfile:
-    data2 = listRID(csvfile)
+    data2 = list_rid(csvfile)
 
-secretStash = ["Giraffes are flamboyant: that is why they never sleep.","There is a legend of a man who knows how to sing.","Once upon a time, there was light. If only that happened once.","If only the hippocampus could fly, we would all by dead by now.","A bug is very much like a human being: it doesn't know why it lives."]
+secretStash = ["Giraffes are flamboyant: that is why they never sleep.",
+               "There is a legend of a man who knows how to sing.",
+               "Once upon a time, there was light. If only that happened once.",
+               "If only the hippocampus could fly, we would all by dead by now.",
+               "A bug is very much like a human being: it doesn't know why it lives."]
 
 # Write dataList rows to output
 try:
     with open(outputList, 'w', newline='') as csvfile:
         outputFile = csv.writer(csvfile)
 
-        '''
-        # Write down headers
-        if header_data1:
-            header1 = data1[0]
-            ridColumn_data1 = findRIDcolumn(data1[0])
-        else:
-            header1 = []
-        if header_data2:
-            header2 = data2[0]
-            ridColumn_data2 = findRIDcolumn(data2[0])
-        else:
-            header2 = []
-        outputFile.writerow(header1 + header2)
-        '''
-
         # Determine column for rid
-        ridColumn_data1 = findRIDcolumn(data1[0],'subjectsList.csv')
-        ridColumn_data2 = findRIDcolumn(data2[0],'dataList.csv')
+        data1_ridColumn = find_column(data1[0], 'rid')
+        data2_ridColumn = find_column(data2[0], 'rid')
+
+        # Determine column for visitCode
+        if compareVisCode:
+            data1_visColumn = find_column(data1[0], 'VISCODE')
+            data2_visColumn = find_column(data2[0], 'VISCODE')
 
         # Actually compare the two lists
-        for rowling in data1: # For each desired subject
+        for rowling in data1:  # For each desired subject
             if not rowling:
                 continue
             found = 0
-            for row in data2:
-                if bleachRID(rowling[ridColumn_data1]) == bleachRID(row[ridColumn_data2]):
-                    found = 1
+            for row in data2:  # For each row in dataList.csv
+                if bleach_rid(rowling[data1_ridColumn]) == bleach_rid(row[data2_ridColumn]):
+                    if compareVisCode:
+                        # noinspection PyUnboundLocalVariable
+                        if rowling[data1_visColumn].strip() == rowling[data2_visColumn].strip():
+                            found = 1
+                    else:
+                        found = 1
+                if found == 1:
                     outputFile.writerow(rowling + row)
                     if not keepAll:
-                        break # Prevent duplicates
-            if skipLineIfNotFound and not found:
-                outputFile.writerow(rowling)
+                        break  # Prevent duplicates
+                if not found:
+                  outputFile.writerow(rowling)
 except PermissionError:
     print('Close the outputList.csv. How do you expect me to work in these conditions geez?')
---
+++ dateCheck.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+__author__ = 'Seqian Wang'
+
+
+def ensure_proper_format(date):
+    # Assumes YYYY MM DD, can get rid of separators
+    date = ''.join(filter(lambda x: x.isdigit(), str(date)))
+    if len(date) != 8:
+        return False
+    format_date = lambda d: d[:4] + "-" + d[4:6] + "-" + d[6:8]
+    date = format_date(date)
+    return date
+
+
+def return_closest_date(dates_list, date):
+    date = ensure_proper_format(date)
+    date_list = [ensure_proper_format(x) for x in dates_list]
+
+    get_datetime = lambda y: datetime.strptime(y, "%Y-%m-%d")
+    closest_date = min(date_list, key=lambda d: abs(get_datetime(d) - get_datetime(date)))
+    return dates_list[closest_date]
