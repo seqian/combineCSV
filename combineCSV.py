0c84f50ffcc8dced20fa9a63127f9ae56202d669
Seqian Wang <seqian@outlook.com>
2014-08-21 18:12:20 -0400

    Generalized the capacity to compare multiple columns in addition to rid. Hasn't been tested yet XD

--- combineCSV.py
+++ combineCSV.py
@@ -4,18 +4,7 @@
 # Version 2014-08-21
 
 ##### Parameters you can have fun playing with
-compareVisCode = True
-keepAll = False
-
-### Options Instructions
-# subjectsList specifies the csv file with the list of subjects.
-# dataList specifies the csv file with the data.
-# outputList specifies the output csv file.
-# compareVisCode. If enabled, it will only match rows with the same visit code. Especially useful with ADNI.
-# keepAll determines whether to keep all the rows in dataList that correspond to a rid from subjectList. \
-# Note that if True you will have to manually choose which row you want to keep and delete.
-# skipLineIfNotFound determines whether to skip lines in outputList to align the rows with rids.
-
+headersToCompare = ['VISCODE', 'EXAMDATE']
 
 ##### The code which you shall not touch without my permission (but feel free to gaze at its ingenuity)
 import csv
@@ -38,17 +27,17 @@ def csv_object(csvfile):
 
 
 def find_column(header, header_string):
-    column_number = 0  # First Column by Default
-    found = False
-    for i in range(len(header)):  # For each column in the header
-        if header_string in header[i].strip().lower():  # If the column's header is RID
-            column_number = i
-            found = True
-            break
-    if not found:
-        column_number = 0  # First Column by Default
-    return column_number
+    for column in range(len(header)):  # For each column in the header
+        if header_string in header[column].strip().lower():  # If the column's header is RID
+            return column
+    return False
 
+def find_rid(header, fileName):
+    column = find_column(header,'rid')
+    if column is False:
+        print("No header with rid in ", fileName, ". I will assume rid's are in the first column.")
+        column = 0  # Assuming First Column
+    return column
 
 def bleach_rid(rid):
     rid = ''.join(i for i in rid if i.isdigit()).lstrip('0')
@@ -79,33 +68,32 @@ try:
     with open(outputList, 'w', newline='') as csvfile:
         outputFile = csv.writer(csvfile)
 
-        # Determine column for rid
-        data1_ridColumn = find_column(data1[0], 'rid')
-        data2_ridColumn = find_column(data2[0], 'rid')
-
-        # Determine column for visitCode
-        if compareVisCode:
-            data1_visColumn = find_column(data1[0], 'VISCODE')
-            data2_visColumn = find_column(data2[0], 'VISCODE')
+        # Determine column for rid and other comparisons
+        data1_ridColumn = find_rid(data1[0],'subjectsList.csv')
+        data2_ridColumn = find_rid(data2[0],'dataList.csv')
+        data1Columns = data2Columns = []
+        for column_header in headersToCompare:
+            data1Columns.append(find_column(data1[0], column_header))
+            data2Columns.append(find_column(data2[0], column_header))
 
         # Actually compare the two lists
-        for rowling in data1:  # For each desired subject
-            if not rowling:
+        for data1_row in data1:  # For each desired subject
+            if not data1_row:
                 continue
             found = 0
-            for row in data2:  # For each row in dataList.csv
-                if bleach_rid(rowling[data1_ridColumn]) == bleach_rid(row[data2_ridColumn]):
-                    if compareVisCode:
-                        # noinspection PyUnboundLocalVariable
-                        if rowling[data1_visColumn].strip() == rowling[data2_visColumn].strip():
-                            found = 1
-                    else:
+            for data2_row in data2:  # For each row in dataList.csv
+                if bleach_rid(data1_row[data1_ridColumn]) == bleach_rid(data2_row[data2_ridColumn]):
+                    if not headersToCompare:
                         found = 1
+                    else:
+                        for i in range(0,len(data1Columns)):
+                            if data1_row[data1Columns[i]].strip() == data1_row[data2Columns[i]].strip():
+                                found = 1
+
                 if found == 1:
-                    outputFile.writerow(rowling + row)
-                    if not keepAll:
-                        break  # Prevent duplicates
-                if not found:
-                  outputFile.writerow(rowling)
+                    outputFile.writerow(data1_row + data2_row)
+                    break
+            if not found:
+                outputFile.writerow(data1_row)
 except PermissionError:
     print('Close the outputList.csv. How do you expect me to work in these conditions geez?')
